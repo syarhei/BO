f010dc8893ebfc112c237877a4652449033f7553
Sergei Murkou <murkous@mail.ru>
2017-05-21 19:37:25 +0300

    heroku

--- utils/session.js
+++ utils/session.js
@@ -10,11 +10,14 @@ module.exports = () => {
         let paths = permissions[client_type];
         let isIn = false;
         for (let path_method in paths) {
-            if (path_method == method && paths.hasOwnProperty(path_method)) {
-                let urls = paths[path_method];
-                for (let url in urls) {
-                    if (urls.hasOwnProperty(url) && urls[url] == path)
-                        isIn = true;
+            if (path_method == method) {
+                if (paths.hasOwnProperty(path_method)) {
+                    let urls = paths[path_method];
+                    for (let url in urls) {
+                        if (urls.hasOwnProperty(url))
+                            if (urls[url] == path)
+                                isIn = true;
+                    }
                 }
             }
         }
